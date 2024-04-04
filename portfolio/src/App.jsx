d93fc9bb65172be8916a79b933737853d019b7fa
kunpaoo <txcokun@gmail.com>
2024-04-05 03:59:40 +0800

    start page

---
+++ portfolio/src/App.jsx
@@ -0,0 +1,40 @@
+
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <div className="h-full flex flex-col items-center justify-center">
+
+
+
+        {/* first screen */}
+      <div className='h-screen w-full flex flex-col items-center justify-center '>
+
+        <div className='max-w-screen w-full flex flex-col items-center'>
+        <div className="title text-10xl font-black tracking-tight shadow-sm w-2/4 text-start self-start slide-left">
+        HELLO!
+        </div>
+        <div className="title text-9xl font-black tracking-tight shadow-sm w-2/4 text-end self-end slide-right">
+        HELLO!
+        </div>
+      </div>
+
+      </div>
+
+
+      <div className='h-screen flex flex-col items-center'>
+        <div className="title w-30 m-auto text-6xl font-black tracking-tight">
+        HELLO
+        </div>
+        <div className="bg-blue-green w-2/4 h-auto p-10 outline outline-red-one outline-offset-2 text-sm shadow-xl text-blue-dark tracking-tight leading-tight font-extrabold">
+          Consequat dolore dolor exercitation in tempor aute pariatur minim tempor excepteur. Culpa et laborum voluptate ut. Dolor sunt est nostrud adipisicing exercitation in adipisicing sunt anim velit ad aliquip magna. Et esse culpa minim tempor velit est et consectetur amet deserunt.
+        </div>
+      </div>
+      </div>
+    </>
+  )
+}
+
+export default App
---
+++ portfolio/tailwind.config.js
@@ -0,0 +1,23 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors:{
+        "cream": "#F2D3D0",
+        "blue-green":"#31AFA4",
+        "red-one":"#DC4361",
+        "blue-main":"#394c99",
+        "blue-dark":"#16223C"
+      },
+      fontSize:{
+        "10xl":"12rem"
+      }
+    },
+  },
+  plugins: [],
+}
+
